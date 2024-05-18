838e8e19b205d039cc9538befecc19a8e8ec9c36
Federico Ramírez <00009220@uca.edu.sv>
2024-05-17 22:00:06 -0600

    feat: migration add estado column to carreers table

---
+++ database/migrations/2024_05_16_215310_add_column_estado_to_carreers_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColumnEstadoToCarreersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('carrera', function (Blueprint $table) {
+            $table->tinyInteger('estado')->after('nombre');
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('carrera', function (Blueprint $table) {
+            $table->dropColumn('estado');
+        });
+    }
+}
