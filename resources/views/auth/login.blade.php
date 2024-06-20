5d6266af6ab8779b58ca17b01649f9f1c9729ccf
Oscar Juarez <00126320@uca.edu.sv>
2024-06-19 23:28:46 -0600

    Fix(Login): browser standard errors

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -39,7 +39,7 @@
                     <div class="container">
                         <div class="card-group">
                             <div id="form-container" class="d-flex flex-column justify-content-center align-items-center mx-auto">
-                                <img class="mx-auto d-block login-logotype" src="img/logotype.svg" />
+                                <img alt="CSS LOGO" class="mx-auto d-block login-logotype" src="img/logotype.svg" />
 
                                 @if ($errors->first('verified'))
                                     {!! $errors->first('verified', '<div class="message_box"> <p style="color: blue">:message</p></div>') !!}
@@ -61,7 +61,7 @@
 
                                     <div class="form-group mb-2">
                                         <div class="d-flex justify-content-between " style="gap: 24px;">
-                                            <label for="con" class="label-form">Contraseña</label>
+                                            <label for="contraseña" class="label-form">Contraseña</label>
                                             <a class="small" href="{{ url('/contra_olvidada_form') }}">Olvidaste tu
                                                 contraseña?</a>
                                         </div>
