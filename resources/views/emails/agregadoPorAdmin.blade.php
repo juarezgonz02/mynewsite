6e0e0d8f24398552d5444eed18f3d4c9cc067040
Oscar Juarez <00126320@uca.edu.sv>
2024-09-04 15:26:24 -0600

    Fix mail: admin add user mail

--- resources/views/emails/agregadoPorAdmin.blade.php
+++ resources/views/emails/agregadoPorAdmin.blade.php
@@ -11,7 +11,7 @@
         <p> Estimado/a {{ $details['user']->nombres }} {{ $details['user']->apellidos }}.</p>
         <p>
             Se le notifica que usted ha sido agregado/a al siguiente proyecto de horas sociales
-            <b> {{$details['user']->nombre}}, por parte del/la encargado/a de dicho proyecto.</b>
+            <b> {{$details['user']->nombre}}</b>, por parte del encargado/a de dicho proyecto.
             Se le solicita estar pendiente de su correo institucional para futuras actualizaciones.<br>
 
             Si usted no solicitó este proceso o si tiene dudas al respecto, por favor ponerse en contacto con nosotros.
